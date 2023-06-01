0ca869865a66187785ec73600595466351e82304
Nikita <nikitafilatov934@gmail.com>
2023-06-01 03:40:33 +0300

    Lots of updates

--- db_connection/main.py
+++ db_connection/main.py
@@ -26,6 +26,7 @@ class Storages(db.Model):
     id_storages = db.Column(db.Integer, primary_key=True)
     name_storages = db.Column(db.String(50), nullable=False)
     address_storages = db.Column(db.String(50), nullable=False)
+    telephone_storages = db.Column(db.String(20), nullable=False)
 
     def __repr__(self):
         return f"<Storages {self.id_storages}>"
@@ -36,6 +37,7 @@ class Producers(db.Model):
     name_producers = db.Column(db.String(50), nullable=False)
     address_producers = db.Column(db.String(50), nullable=False)
     reg_producers = db.Column(db.String(50), nullable=False)
+    telephone_producers = db.Column(db.String(20), nullable=False)
 
     def __repr__(self):
         return f"<Producers {self.id_producers}>"
@@ -55,6 +57,32 @@ class Arrivals(db.Model):
         return f"<Arrivals {self.id_arrivals}>"
 
 
+class Storage1(db.Model):
+    stuffid_storage1 = db.Column(db.Integer, db.ForeignKey('stuff.id_stuff'), primary_key=True)
+    prodid_storage1 = db.Column(db.Integer, db.ForeignKey('producers.id_producers'))
+    quentitystuff_storage1 = db.Column(db.Integer)
+
+    def __repr__(self):
+        return f"<Storage1 {self.stuffid_storage1}>"
+
+
+class Storage2(db.Model):
+    stuffid_storage2 = db.Column(db.Integer, db.ForeignKey('stuff.id_stuff'), primary_key=True)
+    prodid_storage2 = db.Column(db.Integer, db.ForeignKey('producers.id_producers'))
+    quentitystuff_storage2 = db.Column(db.Integer)
+
+    def __repr__(self):
+        return f"<Storage2 {self.stuffid_storage2}>"
+
+
+class Storage3(db.Model):
+    stuffid_storage3 = db.Column(db.Integer, db.ForeignKey('stuff.id_stuff'), primary_key=True)
+    prodid_storage3 = db.Column(db.Integer, db.ForeignKey('producers.id_producers'))
+    quentitystuff_storage3 = db.Column(db.Integer)
+
+    def __repr__(self):
+        return f"<Storage3 {self.stuffid_storage3}>"
+
 
 @app.route('/producers')
 def get_producers():
@@ -62,19 +90,53 @@ def get_producers():
     data.clear()
     for el in info:
         dict_json = {'id_producers': el.id_producers, 'name_producers': el.name_producers,
-                     'address_producers': el.address_producers, 'reg_producers': el.reg_producers}
+                     'address_producers': el.address_producers, 'telephone_producers': el.telephone_producers,
+                     'reg_producers': el.reg_producers, }
         data.append(dict_json)
     articles = jsonify(data)
     return articles
+
+
+@app.route('/producers/adding', methods=['POST'])
+def add_producer():
+    with app.app_context():
+        name_producers = request.json['name_producers']
+        address_producers = request.json['address_producers']
+        reg_producers = request.json['reg_producers']
+        telephone_producers = request.json['telephone_producers']
+        producer = Producers(name_producers=name_producers, address_producers=address_producers,
+                             reg_producers=reg_producers, telephone_producers=telephone_producers)
+        db.session.add(producer)
+        db.session.commit()
 
 
 @app.route('/producers/<int:id_producers>', )
 def get_one_producer(id_producers):
     info_producer = Producers.query.get_or_404(id_producers)
     dict_json = {'id_producers': info_producer.id_producers, 'name_producers': info_producer.name_producers,
-                 'address_producers': info_producer.address_producers, 'reg_producers': info_producer.reg_producers,}
+                 'address_producers': info_producer.address_producers, 'telephone_producers': info_producer.telephone_producers,
+                 'reg_producers': info_producer.reg_producers, }
     articles = jsonify(dict_json)
     return articles
+
+
+@app.route('/producers/<int:id_producers>/put', methods=['PUT'])
+def put_producer(id_producers):
+    info_producer = Producers.query.get_or_404(id_producers)
+    info_producer.name_producers = request.json['name_producers']
+    info_producer.address_producers = request.json['address_producers']
+    info_producer.telephone_producers = request.json['telephone_producers']
+    info_producer.reg_producers = request.json['reg_producers']
+
+    db.session.commit()
+
+
+@app.route('/producers/<int:id_producers>/delete')
+def delete_producer(id_producers):
+    info_producer = Producers.query.get_or_404(id_producers)
+    db.session.delete(info_producer)
+    db.session.commit()
+    return redirect('http://127.0.0.1:5001/producers')
 
 
 @app.route('/storages')
@@ -83,7 +145,7 @@ def get_storages():
     data.clear()
     for el in info:
         dict_json = {'id_storages': el.id_storages, 'name_storages': el.name_storages,
-                     'address_storages': el.address_storages,}
+                     'address_storages': el.address_storages, 'telephone_storages': el.telephone_storages, }
         data.append(dict_json)
     articles = jsonify(data)
     return articles
@@ -92,9 +154,11 @@ def get_storages():
 @app.route('/storages/<int:id_storage>')
 def get_one_storage(id_storage):
     info_storage = Storages.query.get_or_404(id_storage)
+    data.clear()
     dict_json = {'id_storages': info_storage.id_storages, 'name_storages': info_storage.name_storages,
-        'address_storages': info_storage.address_storages, }
-    articles = jsonify(dict_json)
+        'address_storages': info_storage.address_storages, 'telephone_storages': info_storage.telephone_storages}
+    data.append(dict_json)
+    articles = jsonify(data)
     return articles
 
 
@@ -104,7 +168,7 @@ def get_stuff():
     data.clear()
     for el in info:
         dict_json = {'id_stuff': el.id_stuff, 'name_stuff': el.name_stuff, 'category_stuff': el.category_stuff,
-                     'color_stuff': el.reg_producers, 'gender_stuff': el.gender_stuff,
+                     'color_stuff': el.color_stuff, 'gender_stuff': el.gender_stuff,
                      'classifier_stuff': el.classifier_stuff, 'description_stuff': el.description_stuff,
                      }
         data.append(dict_json)
@@ -116,11 +180,48 @@ def get_stuff():
 def get_one_stuff(id_stuff):
     info_stuff = Stuff.query.get_or_404(id_stuff)
     dict_json = {'id_stuff': info_stuff.id_stuff, 'name_stuff': info_stuff.name_stuff, 'category_stuff': info_stuff.category_stuff,
-                 'color_stuff': info_stuff.reg_producers, 'gender_stuff': info_stuff.gender_stuff,
+                 'color_stuff': info_stuff.color_stuff, 'gender_stuff': info_stuff.gender_stuff,
                  'classifier_stuff': info_stuff.classifier_stuff, 'description_stuff': info_stuff.description_stuff,
                  }
     articles = jsonify(dict_json)
     return articles
+
+
+@app.route('/stuff/adding', methods=['POST'])
+def add_stuff():
+    with app.app_context():
+        name_stuff = request.json['name_stuff']
+        category_stuff = request.json['category_stuff']
+        color_stuff = request.json['color_stuff']
+        gender_stuff = request.json['gender_stuff']
+        classifier_stuff = request.json['classifier_stuff']
+        description_stuff = request.json['description_stuff']
+        stuff = Stuff(name_stuff=name_stuff, category_stuff=category_stuff,
+                      color_stuff=color_stuff, gender_stuff=gender_stuff,
+                      classifier_stuff=classifier_stuff, description_stuff=description_stuff)
+        db.session.add(stuff)
+        db.session.commit()
+
+
+@app.route('/stuff/<int:id_stuff>/put', methods=['PUT'])
+def put_stuff(id_stuff):
+    info_stuff = Stuff.query.get_or_404(id_stuff)
+    info_stuff.name_stuff = request.json['name_stuff']
+    info_stuff.category_stuff = request.json['category_stuff']
+    info_stuff.color_stuff = request.json['color_stuff']
+    info_stuff.gender_stuff = request.json['gender_stuff']
+    info_stuff.classifier_stuff = request.json['classifier_stuff']
+    info_stuff.description_stuff = request.json['description_stuff']
+
+    db.session.commit()
+
+
+@app.route('/stuff/<int:id_stuff>/delete')
+def delete_stuff(id_stuff):
+    info_stuff = Stuff.query.get_or_404(id_stuff)
+    db.session.delete(info_stuff)
+    db.session.commit()
+    return redirect('http://127.0.0.1:5001/stuff')
 
 
 @app.route('/arrivals')
@@ -132,6 +233,42 @@ def get_arrivals():
                      'color_stuff': el.reg_producers, 'gender_stuff': el.gender_stuff,
                      'classifier_stuff': el.classifier_stuff, 'description_stuff': el.description_stuff,
                      }
+        data.append(dict_json)
+    articles = jsonify(data)
+    return articles
+
+
+@app.route('/storage/1')
+def get_storage1():
+    info = Storage1.query.order_by(Storage1.stuffid_storage1).all()
+    data.clear()
+    for el in info:
+        dict_json = {'stuffid_storage1': el.stuffid_storage1, 'prodid_storage1': el.prodid_storage1,
+                     'quentitystuff_storage1': el.quentitystuff_storage1,}
+        data.append(dict_json)
+    articles = jsonify(data)
+    return articles
+
+
+@app.route('/storage/2')
+def get_storage2():
+    info = Storage2.query.order_by(Storage2.stuffid_storage2).all()
+    data.clear()
+    for el in info:
+        dict_json = {'stuffid_storage2': el.stuffid_storage2, 'prodid_storage2': el.prodid_storage2,
+                     'quentitystuff_storage2': el.quentitystuff_storage2,}
+        data.append(dict_json)
+    articles = jsonify(data)
+    return articles
+
+
+@app.route('/storage/3')
+def get_storage3():
+    info = Storage3.query.order_by(Storage3.stuffid_storage3).all()
+    data.clear()
+    for el in info:
+        dict_json = {'stuffid_storage3': el.stuffid_storage3, 'prodid_storage3': el.prodid_storage3,
+                     'quentitystuff_storage3': el.quentitystuff_storage3,}
         data.append(dict_json)
     articles = jsonify(data)
     return articles
--- main.py
+++ main.py
@@ -8,7 +8,6 @@ app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:12345@localhost/InterShop'
 db = SQLAlchemy(app)
-data = []
 menu = [{'name': 'Товары', 'url': "stuff"},
         {'name': 'Склады', 'url': "storages"},
         {'name': 'Производители', 'url': "producers"},
@@ -26,10 +25,56 @@ def get_producers():
     return render_template("producers.html", menu=menu, articles=articles)
 
 
+@app.route('/producers/adding', methods=['POST', 'GET'])
+def add_producers():
+    if request.method == 'POST':
+        name_producers = request.form['name']
+        address_producers = request.form['address']
+        telephone_producers = request.form['telephone']
+        reg_producers = request.form['reg']
+        requests.post('http://127.0.0.1:5000/producers/adding', json={'name_producers': name_producers,
+                                                                      'address_producers': address_producers,
+                                                                      'telephone_producers': telephone_producers,
+                                                                      'reg_producers': reg_producers, })
+        return redirect('/producers')
+    else:
+        return render_template("adding/add_producers.html")
+
+
+@app.route('/producers/<int:id_producers>/put', methods=['POST', 'GET'])
+def put_producers(id_producers):
+    info_producer = requests.get(f'http://127.0.0.1:5000/producers/{id_producers}').json()
+    if request.method == 'POST':
+        name_producers = request.form['name']
+        address_producers = request.form['address']
+        telephone_producers = request.form['telephone']
+        reg_producers = request.form['reg']
+        requests.put(f'http://127.0.0.1:5000/producers/{id_producers}/put', json={'name_producers': name_producers,
+                                                                      'address_producers': address_producers,
+                                                                      'telephone_producers': telephone_producers,
+                                                                      'reg_producers': reg_producers, })
+        return redirect('/producers')
+    else:
+        return render_template("putting/put_producers.html", info_producer=info_producer)
+
+
+@app.route('/producers/<int:id_producers>/delete')
+def delete_producer(id_producers):
+    requests.delete(f'http://127.0.0.1:5000/producers/{id_producers}/delete')
+    return redirect('/producers')
+
+
 @app.route('/storages')
 def get_storages():
     articles = requests.get('http://127.0.0.1:5000/storages').json()
     return render_template("storages.html", menu=menu, articles=articles)
+
+
+@app.route('/storages/<int:id_storages>')
+def get_storage(id_storages):
+    storage_info = requests.get(f'http://127.0.0.1:5000/storages/{id_storages}').json()
+    articles = requests.get(f'http://127.0.0.1:5000/storage/{id_storages}').json()
+    return render_template("storage.html", menu=menu, articles=articles, storage_info=storage_info)
 
 
 @app.route('/stuff')
@@ -38,10 +83,82 @@ def get_stuff():
     return render_template("stuff.html", menu=menu, articles=articles)
 
 
+@app.route('/stuff/adding', methods=['POST', 'GET'])
+def add_stuff():
+    if request.method == 'POST':
+        name_stuff = request.form['name']
+        category_stuff = request.form['category']
+        color_stuff = request.form['color']
+        gender_stuff = request.form['gender']
+        classifier_stuff = request.form['classifier']
+        description_stuff = request.form['description']
+        requests.post('http://127.0.0.1:5000/stuff/adding', json={'name_stuff': name_stuff,
+                                                                  'category_stuff': category_stuff,
+                                                                  'color_stuff': color_stuff,
+                                                                  'gender_stuff': gender_stuff,
+                                                                  'classifier_stuff': classifier_stuff,
+                                                                  'description_stuff': description_stuff, })
+        return redirect('/stuff')
+    else:
+        return render_template("adding/add_stuff.html")
+
+
+@app.route('/stuff/<int:id_stuff>/put', methods=['POST', 'GET'])
+def put_stuff(id_stuff):
+    info_stuff = requests.get(f'http://127.0.0.1:5000/stuff/{id_stuff}').json()
+    if request.method == 'POST':
+        name_stuff = request.form['name']
+        category_stuff = request.form['category']
+        color_stuff = request.form['color']
+        gender_stuff = request.form['gender']
+        classifier_stuff = request.form['classifier']
+        description_stuff = request.form['description']
+        requests.put(f'http://127.0.0.1:5000/stuff/{id_stuff}/put', json={'name_stuff': name_stuff,
+                                                                  'category_stuff': category_stuff,
+                                                                  'color_stuff': color_stuff,
+                                                                  'gender_stuff': gender_stuff,
+                                                                  'classifier_stuff': classifier_stuff,
+                                                                  'description_stuff': description_stuff, })
+        return redirect('/stuff')
+    else:
+        return render_template("putting/put_stuff.html", info_stuff=info_stuff)
+
+
+@app.route('/stuff/<int:id_stuff>/delete')
+def delete_stuff(id_stuff):
+    requests.delete(f'http://127.0.0.1:5000/stuff/{id_stuff}/delete')
+    return redirect('/stuff')
+
+
 @app.route('/arrivals')
 def get_arrivals():
     articles = requests.get('http://127.0.0.1:5000/arrivals').json()
     return render_template("arrivals.html", menu=menu, articles=articles)
+
+
+@app.route('/arrivals/adding')
+def add_arrival():
+    info_storages = requests.get('http://127.0.0.1:5000/storages').json()
+    info_stuff = requests.get('http://127.0.0.1:5000/stuff').json()
+    info_producers = requests.get('http://127.0.0.1:5000/producers').json()
+    categories_of_stuff = []
+    data = []
+    for el in info_stuff:
+        categories_of_stuff.append(el['category_stuff'])
+    categories_of_stuff = list(set(categories_of_stuff))
+    for j in range(len(categories_of_stuff)):
+        data_stuff = []
+        for i in range(len(info_stuff)):
+            if info_stuff[i]['category_stuff'] == categories_of_stuff[j]:
+                data_stuff.append(info_stuff[i]['name_stuff'])
+        data.append({'category': categories_of_stuff[j], 'stuff': data_stuff})
+    return render_template("adding/add_arrivals.html", info_storages=info_storages, info_producers=info_producers,
+                           data=data)
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
 
 
 if __name__ == '__main__':
